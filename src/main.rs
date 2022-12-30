1fddf905d9f66d148d0fc69d646c4ba2e051b687
Rex Posadas <rposadas@redwoodlogistics.com>
2022-12-30 10:48:47 +0800

    finished raw mode lesson

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error){
+    panic!("{}", e);
+}
+
 fn main() {
+// the underscore tells the compiler that we want to keep ownership of the value.
+// so it doesn't get removed and we keep raw mode.
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
-     let c = b.unwrap() as char;
-     println!("{}", c);
-     if c == 'q'{
-         break;
-     }
+        match b {
+        Ok(b) =>{
+            let c = b as char;
+            if c.is_control(){
+                println!("{:?} \r", b);
+            } else {
+                println!("{:?} ({}) \r", b, c);
+            }
+            if b == to_ctrl_byte('q') {
+                break;
+            }
+        }
+        Err(err) => die(err),
+        }
     }
 }
