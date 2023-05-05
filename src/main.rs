3732e1d5fe9e185ea7cce92a9dd179d0f24dff55
Rex Posadas <rexposadas@gmail.com>
2023-05-06 01:12:06 +0800

    print ascii value and key

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
 fn main() {
-
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("byte code: {:?} \r", b);
+        } else {
+            println!("byte code: {:?} ({})\r", b, c);
+        }
         if c == 'q' {
             break;
         }
